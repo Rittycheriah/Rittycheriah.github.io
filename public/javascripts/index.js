ae9c33ca8b17f3f20197b4ee73de06f30ca79e00
rittycheriah <rittycheriah@gmail.com>
2015-05-27 19:15:41 -0500

    made views, public, and routes foundational elements

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+
+	angular.module('everyThing', [])
+	  .controller('Main', [$scope, function($scope){
+	  	$scope.name="Dis is my PortFolio";
+	  }])
+})()
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+exports.index = function(req, res){
+	res.render('index', {});
+};
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+exports.partials = function (req, res){
+	res.render('partials/' + req.params.name);
+}
