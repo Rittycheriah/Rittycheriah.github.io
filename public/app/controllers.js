5c4b3c77f6e9e05e8f59fba16a77cce2ab6736a3
rittycheriah <rittycheriah@gmail.com>
2015-05-28 21:04:27 -0500

    added new array to Angular Controller for projs

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -7,6 +7,13 @@ controllers.controller('IndexController', ['$scope', function($scope){
 
 controllers.controller('ProjectsController', ['$scope', function($scope){
 	$scope.message = "Rainu Ittycheriah | Web Developer";
+	$scope.projects = [
+	  {name : 'Dually Quoted',
+     URL : 'heroku', 
+     description : 'An MongoDB, Express, and EJS app designed to keep you inspired. This app stores and creates quotations. Inversely, if you want a quote based on your mood, you can select a mood and the app will randomly populate one for you',
+     contributors : 'Rainu Ittycheriah and Nikki Ahima'
+    }
+	];
 }]);
 
 controllers.controller('ResumeController', ['$scope', function($scope){
